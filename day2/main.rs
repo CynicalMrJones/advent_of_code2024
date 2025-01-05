cb152d615bf5d6c29b78d08b3efcf4a14515bbb4
CynicalMrJones <wopjr1234@gmail.com>
2025-01-05 17:37:17 -0500

    Part 1 done

--- day2/main.rs
+++ day2/main.rs
@@ -20,17 +20,12 @@ fn main() -> Result<()>{
     let test:Vec<_> = result.collect();
 
     let mut answer = 0;
-    for i in 0..200{
-        println!("Index = {}", i+1);
+    for i in 0..1000{
         let test1 = is_acending(&test[i]);
         let test2 = is_decending(&test[i]);
-        println!("{}", test1);
-        println!("{}", test2);
         if test1 | test2{
-            println!("Adding");
             answer = answer + 1;
         }
-        println!("");
     }
 
     println!("Number of Safe = {}", answer);
@@ -39,47 +34,35 @@ fn main() -> Result<()>{
 }
 
 fn is_acending(array: &Vec<&str>) -> bool{
-    for i in 0..array.len() - 1{
-        let current = array[i];
-        let next = array[i+1];
-        // println!("Ascending checking if {} < {}", current, next);
+    for i in 0..array.len()-1{
+        let current: i32 = array[i].parse().unwrap();
+        let next: i32 = array[i+1].parse().unwrap();
         if current < next{
-            let num1: i32 = current.parse().unwrap();
-            let num2: i32 = next.parse().unwrap();
-            let abs = (num1 - num2).abs();
+            let abs = (current - next).abs();
             if (abs >= 4) | (abs == 0) {
-                // println!("False becasue abs = {}", abs);
                 return false;
             }
         }
         else{
-            // println!("False because current !< next");
             return false;
         }
     }
-    // println!("True");
     return true;
 }
 
 fn is_decending(array: &Vec<&str>) -> bool{
-    for i in 0..array.len() - 1{
-        let current = array[i];
-        let next = array[i+1];
-        // println!("Decedning checking if {} > {}", current, next);
+    for i in 0..array.len()-1{
+        let current: i32 = array[i].parse().unwrap();
+        let next: i32 = array[i+1].parse().unwrap();
         if current > next{
-            let num1: i32 = current.parse().unwrap();
-            let num2: i32 = next.parse().unwrap();
-            let abs = (num1 - num2).abs();
+            let abs = (current - next).abs();
             if (abs >= 4) | (abs == 0) {
-                // println!("False becasue abs = {}", abs);
                 return false;
             }
         }
         else{
-            // println!("False because current !> next");
             return false;
         }
     }
-    // println!("True");
     return true;
 }
